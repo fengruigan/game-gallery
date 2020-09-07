da588fd3ba4ebf6d46141d32288a5f571e44aca5
fengruigan <fengruigan@users.noreply.github.com>
2020-09-07 13:37:52 -0400

    successful connect to gc

--- index.js
+++ index.js
@@ -46,18 +46,12 @@ const  { Storage } = require('@google-cloud/storage')
 
 var config = require('./config')
 
-console.log(config)
-// config = JSON.parse(JSON.stringify(config))
-
 const gc = new Storage({
-    // keyFilename: path.join(__dirname, './handy-bonbon-288604-8ff6e1dc52bc.json'),
     credentials: config.credentials,
     projectId: config.projectId
 });
 
-// const gc = new Storage();
-
-gc.getBuckets().then( buckets => console.log(buckets))
+// gc.getBuckets().then( buckets => console.log(buckets))
 
 var storage = multer.diskStorage({
     destination: function(req, file, callback) {
@@ -92,123 +86,123 @@ app.get('/error', (req,res) => {
 })
 
 app.post('/works', upload.array('image', 10), (req,res,next) => {
-    // res.send("此功能在建设中")
+    res.send("此功能在建设中")
 
-    let title = req.body.title;
-    let authors = req.body.author
-    let description = req.body.description
-    let category = req.body.category
-    let event = req.body.event;
-    let imgCount = req.files.length;
-    let download = req.body.download
-    let sections = req.body.section
-    let password = req.body.password
+    // let title = req.body.title;
+    // let authors = req.body.author
+    // let description = req.body.description
+    // let category = req.body.category
+    // let event = req.body.event;
+    // let imgCount = req.files.length;
+    // let download = req.body.download
+    // let sections = req.body.section
+    // let password = req.body.password
 
-    let auth = []
-    if(typeof(authors.name) === "object") {
-        authors.name.forEach( (name, index) => {
-            if (name === "") {
-                name = '无名'
-            }
-            if (authors.position[index] === "") {
-                position = '全能'
-            } else {
-                position = authors.position[index]
-            }
-            auth.push({
-                "name": name,
-                "position": position
-            });
-        });
-    } else {
-        let name = authors.name
-        if (name === "") {
-            name = '无名'
-        }
-        if (authors.position === "") {
-            position = '全能'
-        } else {
-            position = authors.position
-        }
-        auth.push({
-            "name": name,
-            "position": position
-        })
-    }
+    // let auth = []
+    // if(typeof(authors.name) === "object") {
+    //     authors.name.forEach( (name, index) => {
+    //         if (name === "") {
+    //             name = '无名'
+    //         }
+    //         if (authors.position[index] === "") {
+    //             position = '全能'
+    //         } else {
+    //             position = authors.position[index]
+    //         }
+    //         auth.push({
+    //             "name": name,
+    //             "position": position
+    //         });
+    //     });
+    // } else {
+    //     let name = authors.name
+    //     if (name === "") {
+    //         name = '无名'
+    //     }
+    //     if (authors.position === "") {
+    //         position = '全能'
+    //     } else {
+    //         position = authors.position
+    //     }
+    //     auth.push({
+    //         "name": name,
+    //         "position": position
+    //     })
+    // }
 
-    if (category[1] !== "") {
-        category.splice(0,1)
-    } else {
-        category.splice(1,1)
-    }
-    if (event[1] !== "") {
-        event = event[1]
-    } else {
-        event = event[0]
-    }
+    // if (category[1] !== "") {
+    //     category.splice(0,1)
+    // } else {
+    //     category.splice(1,1)
+    // }
+    // if (event[1] !== "") {
+    //     event = event[1]
+    // } else {
+    //     event = event[0]
+    // }
 
-    if (download.link === "") {
-        download.link = "#";
-    }
+    // if (download.link === "") {
+    //     download.link = "#";
+    // }
 
-    let sec = []
-    if (sections !== undefined) {
-        if(typeof(sections.title) === "object") {
-            sections.title.forEach( (title, index) => {
-                let content = section.content[index]
-                content = sanitize(content);
-                if (title === "") {
-                    title = "版块标题"
-                }
-                if (content === "") {
-                    content = "版块内容"
-                }
-                sec.push({
-                    "title": title,
-                    "content": content
-                });
-            });
-        } else {
-            if (sections.title === "") {
-                sections.title = "版块标题"
-            }
-            let content = sections.content
-            if (content === "") {
-                content = "版块内容"
-            }
-            content = sanitize(content);
-            sec.push({
-                "title": sections.title,
-                "content": content
-            })
-        }
-    }
+    // let sec = []
+    // if (sections !== undefined) {
+    //     if(typeof(sections.title) === "object") {
+    //         sections.title.forEach( (title, index) => {
+    //             let content = section.content[index]
+    //             content = sanitize(content);
+    //             if (title === "") {
+    //                 title = "版块标题"
+    //             }
+    //             if (content === "") {
+    //                 content = "版块内容"
+    //             }
+    //             sec.push({
+    //                 "title": title,
+    //                 "content": content
+    //             });
+    //         });
+    //     } else {
+    //         if (sections.title === "") {
+    //             sections.title = "版块标题"
+    //         }
+    //         let content = sections.content
+    //         if (content === "") {
+    //             content = "版块内容"
+    //         }
+    //         content = sanitize(content);
+    //         sec.push({
+    //             "title": sections.title,
+    //             "content": content
+    //         })
+    //     }
+    // }
 
-    let newWork = new Work(
-        title,
-        auth,
-        description,
-        imgCount,
-        category,
-        event,
-        download,
-        sec,
-        password
-    )
+    // let newWork = new Work(
+    //     title,
+    //     auth,
+    //     description,
+    //     imgCount,
+    //     category,
+    //     event,
+    //     download,
+    //     sec,
+    //     password
+    // )
     
-    dbManager.create(newWork);
-    // if no error
-    res.redirect('/success');
+    // dbManager.create(newWork);
+    // // if no error
+    // res.redirect('/success');
 })
 
 app.get('/works/create', (req,res) => {
-    // res.send("此功能在建设中")
-    let categories = db.categories.list;
-    let event = db.event.list
-    let options = Object;
-    options.categories = categories
-    options.event = event
-    res.render('works/create', {options: options});
+    res.send("此功能在建设中")
+    // let categories = db.categories.list;
+    // let event = db.event.list
+    // let options = Object;
+    // options.categories = categories
+    // options.event = event
+    // res.render('works/create', {options: options});
 })
 
 
@@ -219,8 +213,6 @@ app.get('/works/:id', (req,res) => {
     } else {
         let work = dbManager.read(req.params.id);
         if (work) {
-            // res.render("works/" + req.params.id, {work: work});
-            // res.send("Going to render ID " + req.params.id);
             res.render('works/show', {work: work})
         }
     }
@@ -231,9 +223,9 @@ app.get('/works/:id/edit', (req,res) => {
     res.send("此功能还在建设中");
 })
 app.get('/works/:id/delete', (req,res) => {
-    let work = dbManager.read(req.params.id)
-    res.render('works/delete', {work: work})
-    // res.send("此功能还在建设中")
+    // let work = dbManager.read(req.params.id)
+    // res.render('works/delete', {work: work})
+    res.send("此功能还在建设中")
 })
 
 app.delete('/works/:id', (req,res) => {
