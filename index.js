722b3ad654c0c0d2c9845200725a51e78dbc2fe7
fengruigan <fengruigan@users.noreply.github.com>
2020-09-07 11:33:45 -0400

    move credentials to env var

--- index.js
+++ index.js
@@ -31,10 +31,18 @@ const  { Storage } = require('@google-cloud/storage')
 const gc = new Storage({
     // keyFilename: path.join(__dirname, './handy-bonbon-288604-8ff6e1dc52bc.json'),
     credentials: {
-        client_email: process.env.CLIENT_EMAIL,
-        private_key: process.env.PRIVATE_KEY
+        "type": process.env.TYPE,
+        "project_id": process.env.PROJECT_ID,
+        "private_key_id": process.env.PRIVATE_KEY_ID,
+        "private_key": process.env.PRIVATE_KEY,
+        "client_email": process.env.CLIENT_EMAIL,
+        "client_id": process.env.CLIENT_ID,
+        "auth_uri": process.env.AUTH_RUI,
+        "token_uri": process.env.TOKEN_URI,
+        "auth_provider_x509_cert_url": process.env.AUTH_RPOVIDER_X509_CERT_URL,
+        "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
     },
-    projectId: process.env.PROJECT_ID 
+    projectId: process.env.PROJECT_ID
 });
 
 // const gc = new Storage();
